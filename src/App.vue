13363320e249769388a837ddf5d0568bd1043262
maciek01170 <Maciej.Macowicz@gmail.com>
2018-12-03 13:06:12 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <h1>List of attendees</h1>
+    <div id="app">
+      <List :options="options" :customLabel="customLabel">
+        <template slot-scope="{ option }">
+          {{ option.lastName }}, {{ option.firstName }},
+          <a :href="`mailto:${option.email}`">{{ option.email }}</a>
+        </template>
+      </List>
+    </div>
+  </div>
+</template>
+
+<script>
+import List from "./components/List";
+
+export default {
+  name: "App",
+  components: {
+    List
+  },
+  methods: {
+    getemail(option) {
+      return option.email;
+    },
+    customLabel(option) {
+      return (
+        option.lastName +
+        " " +
+        option.firstName +
+        " " +
+        '<a href="mailto:' +
+        option.email +
+        '"> ' +
+        option.email +
+        "</a>"
+      );
+    }
+  },
+  data() {
+    return {
+      options: [
+        {
+          firstName: "John",
+          lastName: "Snow",
+          email: "john@winterfell.com"
+        },
+        {
+          firstName: "Frodo",
+          lastName: "Baggins",
+          email: "frodo@bagend.com"
+        },
+        {
+          firstName: "Lara",
+          lastName: "Croft",
+          email: "lara@tombraider.com"
+        },
+        {
+          firstName: "Clark",
+          lastName: "Kent",
+          email: "clark@dailyplanet.com"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="list">
+    <ul v-for="(option, index) in options" :key="index">
+      <li><slot :option="option" /></li>
+      <!--
+        {{
+          customLabel(option)
+        }}
+      -->
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    options: Array,
+    customLabel: Function
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
